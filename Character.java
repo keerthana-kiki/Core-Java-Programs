5a334ef54560377c7cb49213768c76ccb2e46d15
keerthana-kiki <keerthanag859@gmail.com>
2025-12-05 13:50:17 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,12 @@
+package firstsem;
+
+public class Character {
+	public static void main(String[] args) {
+		System.out.println("give a ASCII value of alphabets");
+		for (char c='a';c<='z';c++) {
+			System.out.println(c+":"+(int)c);
+		}
+		
+	}
+
+}
