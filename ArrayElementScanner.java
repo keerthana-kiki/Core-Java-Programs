47c492d25f392ba8f6f9672d78a58f6b1ea1a2f7
keerthana-kiki <keerthanag859@gmail.com>
2025-12-05 13:52:32 +0530

    Add files via upload

---
+++ ArrayElementScanner.java
@@ -0,0 +1,20 @@
+package firstsem;
+import java.util.Scanner;
+public class ArrayElementScanner {
+	public static void main(String[] args) {
+		Scanner scanner= new Scanner(System.in);
+		System.out.println("Enter the number of elements:");
+		int n=scanner.nextInt();
+		int[] array=new int[n];
+		System.out.println("Enter"+ n +"elements:");
+		for(int i=0;i<n;i++) {
+		array[i]=scanner.nextInt();
+		}
+		System.out.println("Array elements");
+		for(int i=0;i<n;i++) {
+			System.out.println(array[i]+"");
+		}
+	
+	}
+
+}
