c6f4fa7e9206d664015d6ac1f3dabe6364605c3d
keerthana-kiki <keerthanag859@gmail.com>
2025-12-05 13:38:58 +0530

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,20 @@
+package firstsem;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int []arr= {6,5,3,8,9,1,5};
+		int temp;
+		for(int i=0; i < arr.length-1; i++) {
+			for(int j=0; j < arr.length-1; j++) {
+				if(arr[j]>arr[j+1]) {
+					temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(int ele:arr) {
+			System.out.println(ele+"");s
+		}
+	}
+}
