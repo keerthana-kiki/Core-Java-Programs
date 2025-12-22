83ee35ebaac4fcfecdcf9c9850b017857eb3d560
keerthana-kiki <keerthanag859@gmail.com>
2025-12-22 18:31:42 +0530

    Add files via upload

---
+++ Whileloop.java
@@ -0,0 +1,15 @@
+package firstsem;
+import java.util.Scanner;
+public class Whileloop {
+	public static void main(String[] args)
+	{
+		Scanner Scanner= new Scanner(System.in);
+		//TODO Auto-generated method stub
+		int x=10;
+		while(x<20) {
+			System.out.println("x="+x);
+			x++;
+		}
+	}
+
+}
