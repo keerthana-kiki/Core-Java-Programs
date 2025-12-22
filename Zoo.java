8887269179be2bdefdd21943a32e949ada68b68c
keerthana-kiki <keerthanag859@gmail.com>
2025-12-22 18:20:19 +0530

    Add files via upload

---
+++ Zoo.java
@@ -0,0 +1,15 @@
+package firstsem;
+
+public class Zoo {
+	public static void main(String[] args) {
+
+        Animal animal = new Animal();
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        animal.makeSound();
+        dog.makeSound();
+        cat.makeSound();
+    }
+}
+
