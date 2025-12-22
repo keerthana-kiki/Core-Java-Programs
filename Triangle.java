9e949d5fdfed4a3a7e71bb1de1291c5d130b2fff
keerthana-kiki <keerthanag859@gmail.com>
2025-12-22 18:33:35 +0530

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,17 @@
+package firstsem;
+import java.util.Scanner;
+public class Triangle{ 
+	public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("Enter a number ");
+	        int n = sc.nextInt();
+	        for(int i=1;i<n;i++) {
+	        	for(int j=1;j<=n;j++) {
+	        		System.out.println(j);
+	        }
+	        	System.out.println();
+	        }
+}
+
+
+}
